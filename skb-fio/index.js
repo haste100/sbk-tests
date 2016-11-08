61c2245e2a0d2f16cc2557991cd3266d8a7cb76c
haste100 <agudkov@gmail.com>
2016-11-08 21:15:07 +0700

    Initial Revision

--- skb-fio/index.js
+++ skb-fio/index.js
@@ -2,24 +2,34 @@
 var express = require('express');
 var cors = require('cors');
 var app = express();
-function summaNumbers(req, res) {
-    var a = req.query.a;
-    var b = req.query.b;
-    console.log("a=" + a + " b=" + b);
-    var aNum = +a;
-    var bNum = +b;
-    if (isNaN(aNum)) {
-        aNum = 0;
+var ERROR = 'Invalid fullname';
+var WORD_SEPARATOR = ' ';
+function defaultView(req, res) {
+    var fullname = req.query.fullname;
+    console.log("fullname=" + fullname);
+    if (fullname === undefined) {
+        res.send(ERROR);
+        return false;
     }
-    if (isNaN(bNum)) {
-        bNum = 0;
+    var aName = fullname.split(WORD_SEPARATOR);
+    console.log("aName=" + aName);
+    var result = ERROR;
+    if (fullname === '') {
+        result = ERROR;
     }
-    console.log("aNum=" + aNum + " bNum=" + bNum);
-    var sum = aNum + bNum;
-    res.send('' + sum);
+    else if (aName.length === 3) {
+        result = aName[2] + " " + aName[0].substring(0, 1) + ". " + aName[1].substring(0, 1) + ".";
+    }
+    else if (aName.length === 2) {
+        result = aName[1] + " " + aName[0].substring(0, 1) + ".";
+    }
+    else if (aName.length === 1) {
+        result = "" + aName[0];
+    }
+    res.send(result);
 }
 app.use(cors());
-app.get('/task2A', summaNumbers);
+app.get('/task2B', defaultView);
 app.listen(3000, function () {
     console.log('Example app listening on port 3000');
 });
--- skb-fio/index.ts
+++ skb-fio/index.ts
@@ -2,25 +2,33 @@ import * as express from 'express';
 import * as cors from 'cors';
 
 const app = express();
+const ERROR = 'Invalid fullname';
+const WORD_SEPARATOR = ' ';
 
-function summaNumbers(req, res){
-    const a = req.query.a;
-    const b = req.query.b;
-    console.log(`a=${a} b=${b}`);
-    let aNum = +a;
-    let bNum = +b;
-    if (isNaN(aNum)) {
-        aNum = 0;
+function defaultView(req, res){
+    const fullname = req.query.fullname;
+    console.log(`fullname=${fullname}`);
+    if (fullname === undefined) {
+        res.send(ERROR);
+        return false;
     }
-    if (isNaN(bNum)) {
-        bNum=0;
+    const aName = fullname.split(WORD_SEPARATOR);
+    console.log(`aName=${aName}`);
+    let result=ERROR;
+    if (fullname === '') {
+        result=ERROR
+    } else if (aName.length === 3) {
+        result = `${aName[2]} ${aName[0].substring(0,1)}. ${aName[1].substring(0,1)}.`;
+    } else if (aName.length === 2) {
+        result = `${aName[1]} ${aName[0].substring(0,1)}.`;
+    } else if (aName.length === 1) {
+        result = `${aName[0]}`;
     }
-    console.log(`aNum=${aNum} bNum=${bNum}`);
-    const sum=aNum+bNum;
-    res.send(''+sum);
+    res.send(result);
 }
+
 app.use(cors());
-app.get('/task2A', summaNumbers);
+app.get('/task2B', defaultView);
 app.listen(3000, () =>{
     console.log('Example app listening on port 3000');
 });
